c705ce880d35c8a5809e6ba58ce6d70b57ffb44f
Muriiloo <murilooliveira110305@gmail.com>
2025-03-03 14:18:05 -0300

    feat: add transactions pie chart component

--- app/(home)/_components/summary-card.tsx
+++ app/(home)/_components/summary-card.tsx
@@ -16,30 +16,28 @@ const SummaryCard = ({
   size = "small",
 }: SummaryCardProps) => {
   return (
-    <>
-      <Card>
-        <CardHeader className="flex-row items-center gap-4">
-          {icon}
-          <p
-            className={`${size === "small" ? "text-muted-foreground" : "text-white opacity-70"}`}
-          >
-            {title}
-          </p>
-        </CardHeader>
-        <CardContent className="flex justify-between">
-          <p
-            className={`font-bold ${size === "small" ? "text-2xl font-bold" : "text-4xl"}`}
-          >
-            {Intl.NumberFormat("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }).format(amount)}
-          </p>
+    <Card className={`${size === "large" ? "bg-white bg-opacity-5" : ""}`}>
+      <CardHeader className="flex-row items-center gap-4">
+        {icon}
+        <p
+          className={`${size === "small" ? "text-muted-foreground" : "text-white opacity-70"}`}
+        >
+          {title}
+        </p>
+      </CardHeader>
+      <CardContent className="flex justify-between">
+        <p
+          className={`font-bold ${size === "small" ? "text-2xl font-bold" : "text-4xl"}`}
+        >
+          {Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(amount)}
+        </p>
 
-          {size === "large" && <AddTransactionButton />}
-        </CardContent>
-      </Card>
-    </>
+        {size === "large" && <AddTransactionButton />}
+      </CardContent>
+    </Card>
   );
 };
 
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,8 @@ import NavBar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
+import TransactionPieChart from "./_components/transaction-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
 
 interface HomeProps {
   searchParams: {
@@ -23,6 +25,8 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect("/?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <NavBar />
@@ -31,7 +35,15 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text 2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
