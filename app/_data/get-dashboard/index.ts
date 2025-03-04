e6ca5ccf2858febe5bd2906d39d937b9e64fde29
Muriiloo <murilooliveira110305@gmail.com>
2025-03-04 12:22:38 -0300

    feat: add last transaction component

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -72,6 +72,11 @@ export const getDashboard = async (month: string) => {
       (Number(category._sum.amount) / Number(expensesTotal)) * 100,
     ),
   }));
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 10,
+  });
   return {
     balanceTotal,
     depositsTotal,
@@ -79,5 +84,6 @@ export const getDashboard = async (month: string) => {
     expensesTotal,
     typesPercentage,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };
